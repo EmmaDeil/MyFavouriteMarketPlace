0ef21271090727c7bb243de2f2370d3522154399
David Emmanuel <eclefzy@gmail.com>
2025-08-26 16:54:25 +0100

    removed the default stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/logo.ico'
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import PageTitle from './components/PageTitle/PageTitle';
+import PageTitle from './utils/Pagetitle';
 
 function App() {
   
