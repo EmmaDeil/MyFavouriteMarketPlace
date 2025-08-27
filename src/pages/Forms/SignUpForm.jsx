a8636e540d8ea9e31f51d9df71fb8d862ee96efc
David Emmanuel <eclefzy@gmail.com>
2025-08-27 11:26:52 +0100

    I have unpulished this website from github pages

---
+++ src/pages/Forms/SignUpForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUpForm;
